532917f7858bd95be9fa805d73934995a0111b1b
James Zafiri <146983603+zafirij@users.noreply.github.com>
2023-10-07 14:20:33 -0400

    Delete member-search-index.js

--- member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"SortedLinkedList","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"SortedLinkedList","l":"SortedLinkedList()","u":"%3Cinit%3E()"}];updateSearchResults();
